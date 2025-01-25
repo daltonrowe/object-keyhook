d203f70bc51a6c0e25140cc04c13e81e8abcfef3
Dalton <cdaltonrowe@gmail.com>
2025-01-25 09:00:56 +0100

    constants export

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const nothing = 0.03
+
+export const hookRadius = 15;
+export const hookWidth = 5;
+export const hookThickness = 5;
+
+export const postLength = 30;
+
+export const bodySpacing = 50;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,23 @@
 import { cube, union } from "scad-js";
 import hook from "./components/hook.js";
+import { hookRadius, hookWidth, bodySpacing, hookThickness, postLength } from "./constants.js";
 
-const hookSize = 15;
-const hookWidth = 5;
-
-const thickness = 5;
-const spacing = 50;
 
 function hookAndPost() {
   return union(
-    cube([35, hookWidth, thickness]).translate_x(-20),
-    hook(hookWidth, hookSize, 0.3)
+    cube([postLength + hookThickness, hookWidth, hookThickness]).translate_x(-20),
+    hook(hookWidth, hookRadius, 0.3)
       .rotate([0, 90, 0])
-      .translate_z(hookSize / 2 + thickness / 2)
-      .translate_x(-30),
+      .translate_z(hookRadius / 2 + hookThickness / 2)
+      .translate_x(postLength * -1),
   );
 }
 
 export default function () {
   return union(
-    cube([20, 140, thickness]),
+    cube([20, 140, hookThickness]),
     hookAndPost(),
-    hookAndPost().translate_y(spacing * -1),
-    hookAndPost().translate_y(spacing),
+    hookAndPost().translate_y(bodySpacing * -1),
+    hookAndPost().translate_y(bodySpacing),
   );
 }
