0f742471da3fc6d384e798a889a9e14ac1c8eec3
Dalton <cdaltonrowe@gmail.com>
2025-01-25 08:08:32 +0100

    lint

--- src/components/hook.js
+++ src/components/hook.js
@@ -1,13 +1,14 @@
-import { cube, cylinder, difference } from 'scad-js'
-
+import { cube, cylinder, difference } from "scad-js";
 
 export default function (width, size, thickness) {
-  const hole = size - size * thickness
+  const hole = size - size * thickness;
 
   return difference(
     cylinder(width, size).rotate([90, 0, 0]),
     cylinder(width + 2, hole).rotate([90, 0, 0]),
     cube([size * 2, size * 2, size * 2]).translate_x(size),
-    cube([size * 2, size * 2, size * 2]).translate_z(size)
-  ).translate_x(size - size / 2).translate_z(size - size / 2);
+    cube([size * 2, size * 2, size * 2]).translate_z(size),
+  )
+    .translate_x(size - size / 2)
+    .translate_z(size - size / 2);
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
-import { cube, union } from 'scad-js';
-import hook from './components/hook.js';
+import { cube, union } from "scad-js";
+import hook from "./components/hook.js";
 
-const hookSize = 15
-const hookWidth = 5
+const hookSize = 15;
+const hookWidth = 5;
 
-const thickness = 5
-const spacing = 50
-
+const thickness = 5;
+const spacing = 50;
 
 function hookAndPost() {
-  return union(cube([35, hookWidth, thickness]).translate_x(-20),
-    hook(hookWidth, hookSize, .3).rotate([0, 90, 0]).translate_z(hookSize / 2 + thickness / 2).translate_x(-30))
+  return union(
+    cube([35, hookWidth, thickness]).translate_x(-20),
+    hook(hookWidth, hookSize, 0.3)
+      .rotate([0, 90, 0])
+      .translate_z(hookSize / 2 + thickness / 2)
+      .translate_x(-30),
+  );
 }
 
 export default function () {
   return union(
-    cube([20, 140, thickness]), hookAndPost(), hookAndPost().translate_y(spacing * -1), hookAndPost().translate_y(spacing)
-  )
+    cube([20, 140, thickness]),
+    hookAndPost(),
+    hookAndPost().translate_y(spacing * -1),
+    hookAndPost().translate_y(spacing),
+  );
 }
