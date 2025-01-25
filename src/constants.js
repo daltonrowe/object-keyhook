3105ac256b40deef4977536c6ea70df1f2eb7f6b
Dalton <cdaltonrowe@gmail.com>
2025-01-25 09:34:17 +0100

    add holes

--- src/constants.js
+++ src/constants.js
@@ -4,9 +4,10 @@ export const hookRadius = 15;
 export const hookWidth = 5;
 export const hookThickness = 5;
 
-export const postLength = 30;
+export const postLength = 50;
 
 export const bodySpacing = 50;
 
-export const bodyHeight = 20;
+export const bodyHeight = 40;
+export const bodyThickness = 7;
 export const bodyLength = 140;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,39 @@
-import { cube, union } from "scad-js";
+import { cube, cylinder, difference, union } from "scad-js";
 import hook from "./components/hook.js";
 import {
   bodyHeight,
   bodyLength,
   bodySpacing,
+  bodyThickness,
   hookThickness,
-  postLength,
 } from "./constants.js";
 
 function hooks() {
-  return union(hook(),
+  return union(
+    hook(),
     hook().translate_y(bodySpacing * -1),
-    hook().translate_y(bodySpacing))
+    hook().translate_y(bodySpacing),
+  ).translate_z((bodyThickness / 2) * -1 + hookThickness / 2)
+    .translate_x((bodyHeight + hookThickness * 2 - bodyHeight / 2) * -1);
+}
+
+function holes() {
+  return union(
+    cylinder(20, 3).translate_y(bodySpacing / 2),
+    cylinder(20, 3).translate_y((bodySpacing / 2) * -1),
+  );
+}
+
+function body() {
+  return difference(
+    difference(
+      cube([bodyHeight, bodyLength, bodyThickness]),
+      hooks(),
+      holes(),
+    ),
+  );
 }
 
 export default function () {
-  return union(
-    cube([bodyHeight, bodyLength, hookThickness]),
-    hooks().translate_x((bodyHeight + hookThickness) * -1)
-  );
+  return union(body(), hooks())
 }
