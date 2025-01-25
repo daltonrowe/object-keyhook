b10d7817dd84ad6eba2e3f55436885070e2aea23
Dalton <cdaltonrowe@gmail.com>
2025-01-25 16:45:50 +0100

    add comment

--- src/components/hook.js
+++ src/components/hook.js
@@ -18,7 +18,7 @@ function hookAndPost() {
     cube([postLength + hookThickness, hookWidth, hookThickness]),
     hook()
       .rotate([0, 90, 90])
-      .translate_x((postLength / 2) * -1 - hookRadius / 2 - hookRadius / 8)
+      .translate_x((postLength / 2) * -1 - hookRadius / 2 - hookRadius / 8) // hook isn't perfect centered its geometry
       .translate_z(hookRadius),
   );
 }
