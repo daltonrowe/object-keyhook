7b94b423ce87bf33c71f5f99c37602b5df0885db
Dalton <cdaltonrowe@gmail.com>
2025-01-26 20:06:55 +0100

    add jiggle feature

--- src/constants.js
+++ src/constants.js
@@ -1,19 +1,34 @@
+const jiggle = 0; // between 0 and 1
+
+// random move around variables to ensure
+// measurements don't accidentally fit
+function constant(value) {
+  if (!jiggle) return value;
+
+  const rand = value * jiggle;
+  const dir = Math.random() > 0.5 ? 1 : -1;
+
+  return value + rand * dir
+}
+
 export const nothing = 0.03;
 
-export const hookRadius = 15;
-export const hookWidth = 5;
-export const hookThickness = 5;
+export const hookRadius = constant(15);
+export const hookWidth = constant(5);
+export const hookThickness = constant(5);
 
-export const postLength = 30;
+export const postLength = constant(30);
 
-export const bodySpacing = 50;
+export const bodySpacing = constant(50);
 
-export const bodyHeight = 30;
-export const bodyThickness = 6.5;
-export const bodyLength = 140;
+export const bodyHeight = constant(30);
+export const bodyThickness = constant(6.5);
+export const bodyLength = constant(140);
 
-export const padHeight = 16;
-export const padWidth = 24;
-export const padDepth = 1;
+export const padHeight = constant(16);
+export const padWidth = constant(24);
+export const padDepth = constant(1);
 
-export const holeRadius = 1.6;
+export const holeRadius = constant(1.6);
+
+const 
