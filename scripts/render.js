110eb655b36b5ed0dd39bb04c3c0bf2ee1a09889
Dalton <cdaltonrowe@gmail.com>
2025-02-02 10:31:04 +0100

    switch to jiggle add render

--- scripts/render.js
+++ scripts/render.js
@@ -2,4 +2,5 @@ const component = process.argv[2] ? process.argv[2] : "index";
 exec(`node --watch-path=src build.js ${component}`);
 
 const scadPath = path.join(import.meta.dirname, "dist", `${component}.scad`);
-exec(`openscad ${scadPath}`);
+exec(`openscad  -o stl ${scadPath}`);
+exec(`openscad  -o png --viewall ${scadPath}`);
--- src/constants.js
+++ src/constants.js
@@ -1,36 +1,32 @@
-const jiggle = 0; // between 0 and 1
+const jiggleAmount = 0; // between 0 and 1
 
 // random move around variables to ensure
 // measurements don't accidentally fit
-function addJiggle(value) {
-  if (!jiggle) return value;
+function jiggle(value) {
+  if (!jiggleAmount) return value;
 
-  const rand = value * jiggle;
+  const rand = value * jiggleAmount;
   const dir = Math.random() > 0.5 ? 1 : -1;
 
   return value + rand * dir;
 }
 
-const nothing = 0.03;
+export const nothing = 0.03;
 
-const constants = {
-  bodyHeight: 30,
-  bodyLength: 140,
-  bodySpacing: 50,
-  bodyThickness: 6.5,
-  holeRadius: 1.6,
-  holeHeight: bodyThickness + nothing,
-  hookRadius: 15,
-  hookThickness: 5,
-  hookWidth: 5,
-  padDepth: 1,
-  padHeight: 16,
-  padWidth: 24,
-  postLength: 30,
-};
+export const hookRadius = jiggle(15);
+export const hookWidth = jiggle(5);
+export const hookThickness = jiggle(5);
 
-for (const key of constants) {
-  constants[key] = addJiggle(constants[key]);
-}
+export const postLength = jiggle(30);
 
-export default constants;
+export const bodySpacing = jiggle(50);
+
+export const bodyHeight = jiggle(30);
+export const bodyThickness = jiggle(6.5);
+export const bodyLength = jiggle(140);
+
+export const padHeight = jiggle(16);
+export const padWidth = jiggle(24);
+export const padDepth = jiggle(1);
+
+export const holeRadius = jiggle(1.6);
