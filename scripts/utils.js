c514dd16d9099fcc4f36f5e1002b71ea9d72ae8c
Dalton <cdaltonrowe@gmail.com>
2025-02-02 12:48:46 +0100

    progress for publish all

--- scripts/utils.js
+++ scripts/utils.js
@@ -5,7 +5,7 @@ export const srcPath = path.join(import.meta.dirname, "..", "src");
 export const distPath = path.join(import.meta.dirname, "..", "dist");
 
 export function runCommand(command) {
-  const { cmd, args, env, color } = command;
+  const { cmd, args, env = {}, color } = command;
 
   function logColor(data) {
     process.stdout.write(`\x1b[${color}m${data.toString()}\x1b[0m`);
